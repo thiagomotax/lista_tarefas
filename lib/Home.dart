b7e082731f184550d8f8ccc48b527fbe69b39d31
Thiago Mota <thiagomotax@gmail.com>
2020-02-13 21:10:13 -0300

    finalizacao com opção de adicionar e excluir(opção de desfazer exclusão incluída)

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -14,27 +15,60 @@ class _HomeState extends State<Home> {
       []; //manipular apenas a lista para salvar/atualizar/excluir etc
   TextEditingController campoTarefa = TextEditingController();
 
+  _salvarTarefa() {
+    String texto = campoTarefa.text;
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = texto;
+    tarefa["realizada"] = false;
+
+    setState(() {
+      tarefas.add(tarefa);
+      campoTarefa.clear();
+    });
+
+    _salvarArquivo();
+  }
+
   _salvarArquivo() async {
     //assíncriono pois não se sabe quanto tempo demorará para obtêr-se esses dados
     //recuperar local
-    Map<String, dynamic> tarefa = Map();
-    tarefa["titulo"] = "market";
-    tarefa["realizada"] = false;
-    tarefas.add(tarefa);
-
-    final diretorio = await getApplicationDocumentsDirectory();
-    var arquivo = File(diretorio.path + "/" + "dados.json");
+    var arquivo = await _getFile();
     String dados = json.encode(tarefas);
     arquivo.writeAsString(dados);
+  }
+
+  Future<File> _getFile() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+    return File(diretorio.path + "/" + "dados.json");
+  }
+
+  _lerArquivo() async {
+    try {
+      var arquivo = await _getFile();
+      return arquivo.readAsString();
+    } catch (e) {
+      print("erro");
+      return null;
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _lerArquivo().then((dados) {
+      tarefas = json.decode(dados);
+    });
   }
 
   //pega o caminho da pasta de arquivos da plataforma em uso
 
   @override
   Widget build(BuildContext context) {
+    print(tarefas.toString());
     return Scaffold(
       floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         foregroundColor: Colors.white,
@@ -65,7 +99,7 @@ class _HomeState extends State<Home> {
                     FlatButton(
                       child: Text("Ok"),
                       onPressed: () {
-                        _salvarArquivo();
+                        _salvarTarefa();
                         Navigator.pop(context);
                       },
                     ),
@@ -84,9 +118,84 @@ class _HomeState extends State<Home> {
               child: ListView.builder(
                   itemCount: tarefas.length,
                   itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(tarefas[index]),
-                    );
+                    final itemx = tarefas[index];
+                    return Dismissible(
+//                      direction: DismissDirection.endToStart,
+                        onDismissed: (direction) {
+//                          if(direction == DismissDirection.endToStart)
+//                            {
+//
+//                            }
+//                          else if(direction == DismissDirection.startToEnd){
+//
+//                          }
+                          //recuperar ultimo item excluido
+                          dynamic ultimo = tarefas[index];
+
+                          //exclui
+                          setState(() {
+                            tarefas.removeAt(index);
+                          });
+
+                          //snackbar com opção de desfazer
+                          final snackbar = SnackBar(
+                            duration: Duration(seconds: 5),
+                            content: Text("Tarefa removida"),
+                            action: SnackBarAction(
+                              label: "Desfazer",
+                              onPressed: (){
+                                  setState(() {
+                                    tarefas.add(ultimo);
+                                  });
+                              },
+                            ),
+                          );
+                          Scaffold.of(context).showSnackBar(snackbar);
+
+
+
+                          print("oi ${direction}");
+                          print("deletado: ${tarefas[index]}" );
+
+                        },
+//                        background: Container(
+//                          color: Colors.green,
+//                          padding: EdgeInsets.all(16),
+//                          child: Row(
+//                            mainAxisAlignment: MainAxisAlignment.start,
+//                            children: <Widget>[
+//                              Icon(
+//                                Icons.edit,
+//                                color: Colors.white,
+//                              )
+//                            ],
+//                          ),
+//                        ),
+                        background: Container(
+                          color: Colors.red,
+                          padding: EdgeInsets.all(16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        key: Key(itemx.toString()),
+                        child: CheckboxListTile(
+                          value: tarefas[index]["realizada"],
+                          onChanged: (valor) {
+                            setState(() {
+                              tarefas[index]["realizada"] = valor;
+                              _salvarArquivo();
+                            });
+                          },
+                          title: Text(tarefas[index]["titulo"]),
+                        ));
                   })),
         ],
       ),
